8b427ea14d4795fa41d57acd240b0b177e1ff84e
Adrian Serrano <adriansb3105@gmail.com>
2018-05-20 22:39:46 -0600

    Bug fixed adding a Service provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LibroService } from './model/libro.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,10 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     HttpClientModule,
     BrowserModule,
+    HttpModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ LibroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
