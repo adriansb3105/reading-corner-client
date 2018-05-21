e80101d1511cda0b551bb33fa72792710867efdd
Adrian Serrano <adriansb3105@gmail.com>
2018-05-21 00:21:12 -0600

    API gets consumed correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -17,6 +18,7 @@ import { LibroService } from './model/libro.service';
     HttpClientModule,
     BrowserModule,
     HttpModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [ LibroService ],
--- src/app/model/libro.service.ts
+++ src/app/model/libro.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers } from "@angular/http";
 import { Observable } from "rxjs";
 import { map } from 'rxjs/operators';
 
@@ -12,9 +12,17 @@ export class LibroService {
   private url = 'http://localhost:8080/api/buscar';
 
     constructor(private http: Http){
+        const headers = new Headers();
+        headers.append('Access-Control-Allow-Headers', 'Content-Type');
+        headers.append('Access-Control-Allow-Methods', 'GET');
+        headers.append('Access-Control-Allow-Origin', '*');
     }
     
     findLibrosByTituloYPublicador(libro:string, publicador:string): Observable<Generic[]> {
         return this.http.get(this.url + '/' + libro + '/' + publicador).pipe(map(response => response.json()))
     }
+
+    //findLibrosByTituloYPublicador(libro:string, publicador:string): void {
+    //    this.http.get(this.url + '/' + publicador + '/' + libro).subscribe(res => console.log(res));
+    //}
 }
